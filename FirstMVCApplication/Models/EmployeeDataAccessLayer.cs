ee50fefd99f4c33a96c522b66f752cbffd04d342
Sumit Pal <mr.pal92@gmail.com>
2018-10-05 14:37:08 +0530

    Getting Started and Setting up the files

---
+++ FirstMVCApplication/Models/EmployeeDataAccessLayer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstMVCApplication.Models
+{
+    public class EmployeeDataAccessLayer
+    {
+        string connectionString = "Data Source = PAL-PC; Initial Catalog = EmployeeDB; Integrated Security = true;";
+
+        public IEnumerable<Employee> GetAllEmployees()
+        {
+
+        }
+
+    }
+}
